f3fae851c4b7f2fc7bfd457b9b0e7e8505251e23
Muhanned Noman <Muhanned.Noman@gmail.com>
2020-08-30 07:28:17 +0300

    Testing numbers

--- tests/lib.test.js
+++ tests/lib.test.js
@@ -1,3 +1,18 @@
-test('First Test', () => {
-  throw new Error('Something is Wrong');
+const lib = require('../lib');
+
+describe('absolute', () => {
+  it('should return a postive number if input is positive', () => {
+    const result = lib.absolute(1);
+    expect(result).toBe(1);
+  });
+
+  it('should return a positive number if input is negative', () => {
+    const result = lib.absolute(-1);
+    expect(result).toBe(1);
+  });
+
+  it('should return zero if input is zero', () => {
+    const result = lib.absolute(0);
+    expect(result).toBe(0);
+  });
 });
