b3c03a41c86f1b14ec231e8d625bc43300f60204
Muhanned Noman <Muhanned.Noman@gmail.com>
2020-08-30 07:35:57 +0300

    Testing strings

--- tests/lib.test.js
+++ tests/lib.test.js
@@ -16,3 +16,11 @@ describe('absolute', () => {
     expect(result).toBe(0);
   });
 });
+
+describe('greet', () => {
+  it('should return the greeting message', () => {
+    const result = lib.greet('Muhanned');
+    expect(result).toMatch(/Muhanned/);
+    expect(result).toContain('Muhanned');
+  });
+});
