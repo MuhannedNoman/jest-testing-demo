0c2db52546bed88abe4f43b82b969150714ab2f5
Muhanned Noman <Muhanned.Noman@gmail.com>
2020-08-30 08:00:29 +0300

    Testing throw

--- tests/lib.test.js
+++ tests/lib.test.js
@@ -40,3 +40,14 @@ describe('getProduct', () => {
     expect(result).toHaveProperty('id', 1);
   });
 });
+
+describe('registerUser', () => {
+  it('should throw if username is falsy', () => {
+    const args = [null, undefined, NaN, '', 0, false];
+    args.forEach((a) => {
+      expect(() => {
+        lib.registerUser(a);
+      }).toThrow();
+    });
+  });
+});
